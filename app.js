a120b9c0143839c417b75398929edb0e225d2692
Farah Elkourdi <82680705+felkourdi@users.noreply.github.com>
2022-03-26 22:02:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ function startKeepAlive() {
 
 function sendEmail() {
   // runs at 6 pm NJ time , 22 on server time 
-  cron.schedule('0 02 * * *', () => {
+  cron.schedule('0 03 * * *', () => {
+   console.log("test2");
     items.getitems();
   });
 }
